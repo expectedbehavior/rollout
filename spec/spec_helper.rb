70056c9c88123529d7617312c325f02f9a784303
Kristopher Chambers <kristopher.chambers@gmail.com>
2010-12-22 03:10:17 -0500

    Defaulting to rspec mocks. Requiring memcached in the spec helper Flushing memcache before each spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,22 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
+require 'bundler/setup'
 require 'rollout'
 require 'spec'
 require 'spec/autorun'
 require 'bourne'
 require 'redis'
+require 'memcached'
+
+$memcache_config = {
+  :servers => ['localhost:11211']
+}
 
 Spec::Runner.configure do |config|
-  config.mock_with :mocha
-  config.before { Redis.new.flushdb }
+  config.mock_with :rspec
+  config.before do
+    Redis.new.flushdb 
+    Memcached::Rails.new($memcache_config[:servers]).flush_all
+  end
 end
